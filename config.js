3e955e090595354e94eed657ca599247ed00a8c2
chenhonzhou <m15107415590@163.com>
2017-10-26 09:44:38 +0800

    branch develop

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var _config = {
-    blog_name       : 'QQ空间',         // 博客名称
+    blog_name       : '哔哩哔哩 (゜-゜)つロ',         // 博客名称
     owner           : 'chenhonzhou',           // github 用户名
-    repo            : 'Develop-Issues-blog',// github 中对应项目名
-    per_page        : '15'                    // 默认一页显示几篇文章
+    repo            : 'Develop-Life-blog',// github 中对应项目名
+    per_page        : '10'                    // 默认一页显示几篇文章
 }
 
 var duoshuoQuery = {short_name:_config['duoshuo_id']};
