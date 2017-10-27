48e06249fe990784690eb1bc63e1bce28dd00e02
chenhonzhou <chenhonzhou@gmail.com>
2017-10-27 12:14:12 +0800

    page-be-1

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var _config = {
     blog_name       : '哔哩哔哩 (゜-゜)つロ',         // 博客名称
     owner           : 'chenhonzhou',           // github 用户名
     repo            : 'Develop-Life-blog',// github 中对应项目名
-    per_page        : '10'                    // 默认一页显示几篇文章
+    per_page        : '10',                    // 默认一页显示几篇文章
+    access_token: '',
 }
 
 var duoshuoQuery = {short_name:_config['duoshuo_id']};
