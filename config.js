a772f8c171aac3000382efc0c65edf5792d70918
chenhonzhou <chenhonzhou@gmail.com>
2017-10-30 16:19:59 +0800

    美化

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var _config = {
-    blog_name       : '哔哩哔哩 (゜-゜)つロ',         // 博客名称
+    blog_name       : '陈宏洲',         // 博客名称
     owner           : 'chenhonzhou',           // github 用户名
     repo            : 'Develop-Life-blog',// github 中对应项目名
     per_page        : '10',                    // 默认一页显示几篇文章
