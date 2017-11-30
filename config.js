44f3d0fbc20d3981b7126b6df889a7a4f46db365
chenhonzhou <m15107415590@163.com>
2017-11-30 14:23:04 +0800

    an...

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var _config = {
     blog_name: '陈宏洲的博客',         // 博客名称
     owner: 'chenhonzhou',           // github 用户名
     repo: 'Develop-Life-blog',// github 中对应项目名
-    per_page: '10',                    // 默认一页显示几篇文章
+    per_page: '6',                    // 默认一页显示几篇文章
     access_token: '',
 }
