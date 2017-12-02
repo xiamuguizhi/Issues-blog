77459bdf11eb8382ca1866ca93837741d024d925
chenhonzhou <m15107415590@163.com>
2017-12-02 19:24:52 +0800

    api...

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var _config = {
-    blog_name: '陈宏洲的博客',         // 博客名称
-    owner: 'chenhonzhou',           // github 用户名
-    repo: 'life-blog',// github 中对应项目名
-    per_page: '10',                    // 默认一页显示几篇文章
-    access_token: 'faa339cfbd31a9614a47a340b5f313dd5a09fb28',
+    blog_name       : '陈宏洲的博客',         // 博客名称
+    owner           : 'chenhonzhou',           // github 用户名
+    repo            : 'blog',// github 中对应项目名
+    //duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
+    //access_token    : 'myblog'+'api',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+    per_page        : '10'                    // 默认一页显示几篇文章
 }
