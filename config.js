54f55c4b0718a61a035db635a07322dd7b545ab7
chenhonzhou <m15107415590@163.com>
2017-12-02 18:47:26 +0800

    B-T-F-Fucking

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name: '陈宏洲的博客',         // 博客名称
     owner: 'chenhonzhou',           // github 用户名
-    repo: 'Develop-Life-blog',// github 中对应项目名
-    per_page: '6',                    // 默认一页显示几篇文章
-    access_token: '',
+    repo: 'life-blog',// github 中对应项目名
+    per_page: '10',                    // 默认一页显示几篇文章
+    access_token: 'faa339cfbd31a9614a47a340b5f313dd5a09fb28',
 }
