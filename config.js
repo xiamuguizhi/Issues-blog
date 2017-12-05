bdba5c1265fc3d118548aeb7512d7e3505a43061
chenhonzhou <m15107415590@163.com>
2017-12-05 22:14:54 +0800

    page.40.yes

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ var _config = {
     repo            : 'blog',// github 中对应项目名
     //duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
     //access_token    : 'myblog'+'api',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
-    per_page        : '10'                    // 默认一页显示几篇文章
+    per_page        : '40'                    // 默认一页显示几篇文章
 }
