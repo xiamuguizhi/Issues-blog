d0f89fca7e60ca3fd21af1b5c1953889470e8539
chenhonzhou <m15107415590@163.com>
2018-01-01 22:16:18 +0800

    con

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name       : "YoungDog'Blog",         // 博客名称
     owner           : 'chenhonzhou',           // github 用户名
-    repo            : 'note-blog',// github 中对应项目名
+    repo            : 'issues-blog',// github 中对应项目名
     access_token    : '00ee4de710835843388b' + '36b669a72c2c1f718841',
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     per_page        : '10'                    // 默认一页显示几篇文章
